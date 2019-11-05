3f589e04a40c91b7d6621030ae1b6ce9707be51a
wil-g2 <wil-g2@hotmail.com>
2019-11-05 18:24:24 -0300

    chore: create test using jest.

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,13 @@
+const { User } = require('../../src/app/models');
+
+describe("Authentication", () => {
+  it("should sum two numbers", async () =>{
+    const user = await User.create({
+      name: 'willian', 
+      email:'wil-g2@hotmail.com', 
+      password_hash: '123456'
+    }); 
+    console.log(user);
+    expect(user.email).toBe('wil-g2@hotmail.com');
+  });
+});
