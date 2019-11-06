4c8ea10889476ca325485402e3d7dc68c1f63054
wil-g2 <wil-g2@hotmail.com>
2019-11-06 12:39:42 -0300

    chore: create integration test to user methods.

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,13 +1,68 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
 const { User } = require('../../src/app/models');
+const truncate = require('../utils/truncate');
 
 describe("Authentication", () => {
-  it("should sum two numbers", async () =>{
+  beforeEach(async () => {
+    await truncate();
+  });
+
+
+  it("should authenticate with valid credentials", async () =>{
     const user = await User.create({
       name: 'willian', 
       email:'wil-g2@hotmail.com', 
       password_hash: '123456'
     }); 
-    console.log(user);
-    expect(user.email).toBe('wil-g2@hotmail.com');
+
+    const response = await request(app)
+    .post("/sessions")
+    .send({
+      email: user.email, 
+      password: "123456"
+    }); 
+
+    expect(response.status).toBe(200);
+   
   });
+
+
+  it("should not authenticate with invalid credentials", async () =>{
+    const user = await User.create({
+      name: 'willian', 
+      email:'wil-g2@hotmail.com', 
+      password_hash: '123456'
+    }); 
+
+    const response = await request(app)
+    .post("/sessions")
+    .send({
+      email: user.email, 
+      password: "1234567"
+    }); 
+
+    expect(response.status).toBe(401);
+   
+  });
+
+
+  it("should return jwt token when authenticated", async () => {
+    const user = await User.create({
+      name: 'willian', 
+      email:'wil-g2@hotmail.com', 
+      password_hash: '123456'
+    }); 
+
+    const response = await request(app)
+    .post("/sessions")
+    .send({
+      email: user.email, 
+      password: "123456"
+    }); 
+
+    expect(response.status).toBe(200);
+  }); 
+
 });
