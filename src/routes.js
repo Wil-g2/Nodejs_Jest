d238518ac0397427b3c3fc2f761d10e0a25f4a18
wil-g2 <wil-g2@hotmail.com>
2019-11-06 12:36:19 -0300

    feat: create route sessions to method post.

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const routes = require('express').Router();
+const SessionController = require('../src/app/controllers/SessionController');
 
-
+routes.post("/sessions", SessionController.store);
 
 module.exports = routes;
